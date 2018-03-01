bfa0e68abc9295326ba78099b3b8890d140504b2
frozenbanana <henrybergstrom1@gmail.com>
2018-03-01 15:09:10 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <GL/glew.h>    // include GLEW and new version of GL on Windows
+#include <GLFW/glfw3.h> // GLFW helper library
+#include <stdio.h>
+#include <iostream>
+
+int main() {
+
+  return 0;
+}
